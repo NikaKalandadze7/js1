2d049f30a74e175870cce46ac8aa06b62ebae35f
NikaKalandadze7 <nika.kalandadze71@gmail.com>
2024-10-21 18:22:16 +0400

    1

---
+++ script.js
@@ -0,0 +1,15 @@
+let enterScore = prompt("Please Enter your score");
+
+let score = parseInt(enterScore);
+
+if (score >= 90) {
+  console.log("A");
+} else if (score >= 80) {
+  console.log("B");
+} else if (score >= 70) {
+  console.log("C");
+} else if (score >= 60) {
+  console.log("D");
+} else {
+  console.log("F");
+}
